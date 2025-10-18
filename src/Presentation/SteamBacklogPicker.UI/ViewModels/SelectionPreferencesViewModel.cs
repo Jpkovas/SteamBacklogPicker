ec556f8fac8c5147b61c63eb2a0920aba6a56b4d
João Karalkovas <therussogames@gmail.com>
2025-10-18 14:44:14 -0300

    Respect deselected Deck compatibility filters

--- src/Presentation/SteamBacklogPicker.UI/ViewModels/SelectionPreferencesViewModel.cs
+++ src/Presentation/SteamBacklogPicker.UI/ViewModels/SelectionPreferencesViewModel.cs
@@ -465,11 +465,6 @@ public sealed class SelectionPreferencesViewModel : ObservableObject
                 allowed |= DeckCompatibilityFilter.Unsupported;
             }
 
-            if (allowed == DeckCompatibilityFilter.None)
-            {
-                allowed = DeckCompatibilityFilter.All;
-            }
-
             p.Filters.AllowedDeckCompatibility = allowed;
         });
     }
